bf610e9f59a8454567b94922d47b560ed7d05716
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-19 13:53:28 +0300

    Comment to commit

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://phasmaimmersion.nomoredomainswork.ru/api'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/local`,
+  me: `${BASE_URL}/users/me`,
+}
