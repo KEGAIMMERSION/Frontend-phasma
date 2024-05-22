f96e354a0853aa25d06109e599f388872d4e8c88
KEGAIMMERSION <dmitriydimon623@gmail.com>
2024-05-22 23:00:27 +0300

    main

--- app/api/config.js
+++ app/api/config.js
@@ -3,5 +3,5 @@ export const BASE_URL = 'https://phasmaimmersion.nomoredomainswork.ru/api'
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/users/me`,
+  me: `${BASE_URL}/me`,
 }
